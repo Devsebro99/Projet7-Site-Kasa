7d9478f3979aed9596bd35f160731b1899788371
Devsebro99 <100680925+Devsebro99@users.noreply.github.com>
2023-04-19 17:10:54 +0200

    fin de verification et du projet 7

--- frontend/src/components/slideShow/SlideShow.jsx
+++ frontend/src/components/slideShow/SlideShow.jsx
@@ -5,25 +5,36 @@ function ImageSlider(props) {
   const numberLength = props.loge.pictures.length
   const [currentImage, setCurrentImage] = useState(0)
 
-  const handlePreviousClick = () => {
+  const PreviousClick = () => {
     const previousImage = currentImage - 1
     const imageNumber = previousImage < 0 ? numberLength - 1 : previousImage
     setCurrentImage(imageNumber)
   }
 
-  const handleNextClick = () => {
+  const NextClick = () => {
     const nextImage = currentImage + 1
     const imageNumber = nextImage > numberLength - 1 ? 0 : nextImage
     setCurrentImage(imageNumber)
   }
 
-  return (
-    <article className="loge_image">
-      <button onClick={handlePreviousClick} className="previousClick"></button>
-      <img src={props.loge.pictures[currentImage]} alt="Logement" />
-      <button onClick={handleNextClick} className="nextClick"></button>
-    </article>
-  )
+  if (numberLength === 1) {
+    return (
+      <article className="loge_image">
+        <img src={props.loge.pictures[currentImage]} alt="Logement" />
+      </article>
+    )
+  } else {
+    return (
+      <article className="loge_image">
+        <button onClick={PreviousClick} className="previousClick"></button>
+        <img src={props.loge.pictures[currentImage]} alt="Logement" />
+        <div className="numberPage">
+          {[currentImage + 1]} / {[numberLength]}
+        </div>
+        <button onClick={NextClick} className="nextClick"></button>
+      </article>
+    )
+  }
 }
 
 export default ImageSlider
